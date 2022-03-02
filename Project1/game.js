f5dc4d305a03c2993956c9983438e3871eda1031
Chris Marcial <toastysnickers@gmail.com>
2022-03-01 20:05:37 -0700

    update game js

--- Project1/game.js
+++ Project1/game.js
@@ -1,14 +1,46 @@
-//const readlineSync = require('readline-sync');
-import readlineSync from 'readline-sync';
-//const chalk = require('chalk');
+//import readlineSync from 'readline-sync';
+
 import chalk from 'chalk';
-const log = console.log;
 
-var user, pw, command;
+//const log = console.log;
 
-log(chalk, black, bold, bgYellow('  Your Account  '));
-user = readlineSync.question(chalk.gray.underline(' USER NAME ') + ' : ');
-pw = readlineSync.question(chalk.gray.underline(' PASSWORD ') + ' : ', {hideEchoBack: true});
-// Authorization ...
-log(chalk.green('Welcome, ' + user +'!'));
-command = readlineSync.prompt();
+import {TicTacToe} from './functions.js'
+
+//import readline from 'readline';
+
+import input from 'readline-sync';
+
+import fs from 'fs';
+
+//START PROGRAM
+console.log(chalk.blue(`Would you like to play tic tac toe? (2 players)`))
+
+let answer = input.question("Y or N: ");
+
+if (answer == "Y" || answer == "y") // let's play
+{
+    let player1Name= input.question(chalk.cyan("What is player 1's name? "));
+    const player1 = `Player 1: ${player1Name}`;
+
+    fs.writeFile('./input.json', player1, err => {
+        if (err) throw err;
+        // if not, successfully written
+      })
+
+    let player2Name= input.question(chalk.magenta("What is player 2's name? "));
+    const player2 = `\nPlayer 2: ${player2Name}`;
+
+    fs.appendFile("./input.json", player2, err => {
+        if(err) throw err;
+       // if not, successfully written
+    });
+
+    // GAME START
+    var game = new TicTacToe();
+    console.log(chalk.blue("Player 2, " + player2Name + ", go first:"));
+    game.gameStart();
+}
+else if (answer == "N" || answer == "n") // don't play
+{
+    console.log(chalk.yellow('Exiting game...'));
+}
